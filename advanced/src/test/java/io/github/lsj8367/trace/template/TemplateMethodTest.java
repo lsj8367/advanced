656843aa9a8821cf063fad42286f7352d59c81e4
lsj8367 <lsj8367@naver.com>
2022-02-16 23:36:55 +0900

    :sparkles: 템플릿 메소드 패턴 테스트

--- advanced/src/test/java/io/github/lsj8367/trace/template/TemplateMethodTest.java
+++ advanced/src/test/java/io/github/lsj8367/trace/template/TemplateMethodTest.java
@@ -34,4 +34,12 @@ public class TemplateMethodTest {
         log.info("resultTime={}", resultTime);
     }
 
+    // 템플릿 메소드 패턴 적용
+    @Test
+    void templateMethodV1() {
+        AbstractTemplate template1 = new SubClassLogic1();
+        template1.execute();
+        AbstractTemplate template2 = new SubClassLogic2();
+        template2.execute();
+    }
 }
