10b0b8d5f76eb72b33a4cefd93a3c234aa023365
lsj8367 <lsj8367@naver.com>
2022-02-24 23:22:39 +0900

    :sparkles: V1 Controller, Service, Repository 예제 실습

---
+++ proxy/src/main/java/hello/proxy/app/OrderRepositoryV1.java
@@ -0,0 +1,7 @@
+package hello.proxy.app;
+
+public interface OrderRepositoryV1 {
+
+    void save(final String itemId);
+
+}
---
+++ proxy/src/main/java/hello/proxy/app/OrderServiceV1.java
@@ -0,0 +1,5 @@
+package hello.proxy.app;
+
+public interface OrderServiceV1 {
+    void orderItem(final String itemId);
+}
