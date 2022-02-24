02bda7c4d137a4e9460cafa612c9d817b73977ad
lsj8367 <lsj8367@naver.com>
2022-02-24 23:53:41 +0900

    :sparkles: V3구현, (v1,v2) import등록

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,11 +1,13 @@
 package hello.proxy;
 
 import hello.proxy.configuration.AppV1Configuration;
+import hello.proxy.configuration.AppV2Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(AppV1Configuration.class) //클래스를 스프링 빈으로 등록
+//@Import(AppV1Configuration.class)
+@Import({AppV1Configuration.class, AppV2Configuration.class}) //클래스를 스프링 빈으로 등록
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
