4a4bdb46e083cafd7c4d9126ce26cbd38a5014fe
lsj8367 <lsj8367@naver.com>
2022-02-24 23:23:43 +0900

    :sparkles: 스캔하지 않는 패키지의 클래스 import

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,13 +1,16 @@
 package hello.proxy;
 
+import hello.proxy.configuration.AppV1Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+@Import(AppV1Configuration.class) //클래스를 스프링 빈으로 등록
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProxyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProxyApplication.class, args);
+    }
 
 }
