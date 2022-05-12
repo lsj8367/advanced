13b48f18c9b9d6778a67f6a6de81ceb476fef91d
lsj8367 <lsj8367@naver.com>
2022-05-13 00:28:40 +0900

    :sparkles: 기본설정 cglib

--- aop/src/test/java/io/github/lsj8367/aop/proxyvs/code/ProxyCastingTest.java
+++ aop/src/test/java/io/github/lsj8367/aop/proxyvs/code/ProxyCastingTest.java
@@ -10,9 +10,10 @@ import org.springframework.context.annotation.Import;
 
 @Slf4j
 //@SpringBootTest(properties = {"spring.aop.proxy-target-class=false"}) //JDK 동적 프록시
-@SpringBootTest(properties = {"spring.aop.proxy-target-class=true"}) //CGLIB 동적 프록시
+//@SpringBootTest(properties = {"spring.aop.proxy-target-class=true"}) //CGLIB 동적 프록시
+@SpringBootTest
 @Import(ProxyDiAspect.class)
-public class ProxyCastingTest {
+class ProxyCastingTest {
 
     @Autowired
     MemberService memberService;
