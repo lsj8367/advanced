d9005d176b888b6ca42826fd0fd3efd71a458ea0
lsj8367 <lsj8367@naver.com>
2022-05-12 22:26:57 +0900

    :sparkles: 멤버 aop

---
+++ aop/src/main/java/io/github/lsj8367/aop/member/MemberService.java
@@ -0,0 +1,5 @@
+package io.github.lsj8367.aop.member;
+
+public interface MemberService {
+    String hello(String param);
+}
---
+++ aop/src/main/java/io/github/lsj8367/aop/member/MemberServiceImpl.java
@@ -0,0 +1,20 @@
+package io.github.lsj8367.aop.member;
+
+import io.github.lsj8367.aop.member.annotation.ClassAop;
+import io.github.lsj8367.aop.member.annotation.MethodAop;
+import org.springframework.stereotype.Component;
+
+@ClassAop
+@Component
+public class MemberServiceImpl implements MemberService {
+
+    @Override
+    @MethodAop("test value")
+    public String hello(String param) {
+        return "ok";
+    }
+
+    public String internal(String param) {
+        return "ok";
+    }
+}
